42856d603ef7e773aa32863b4ee4613321a1165e
HamSudo <192023912+HamSudo@users.noreply.github.com>
2025-08-01 01:18:42 +0400

    Add the main function playGame() to play 5 rounds

--- main.js
+++ main.js
@@ -13,24 +13,27 @@ function getHumanChoice () {
     return prompt("What would you choose: rock, paper, or scissors?");
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound (humanChoice , computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+    function playRound (humanChoice , computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
 
-    if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
-    } else if (humanChoice === computerChoice) {
-        console.log(`Draw! ${humanChoice} doesn't beat itself`);
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore += 1;
+        if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1;
+        } else if (humanChoice === computerChoice) {
+            console.log(`Draw! ${humanChoice} doesn't beat itself`);
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore += 1;
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice()); 
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection , computerSelection);
+playGame();
